61b505e5ae9e737f43c98476595e867c00198472
william <wwwy3y3@gmail.com>
2015-03-15 21:32:38 +0800

    add session

--- test/api.js
+++ test/api.js
@@ -2,11 +2,13 @@ var path= require('path');
 var express= require('express');
 var should= require('should');
 var request= require('supertest');
+var cookieParser = require('cookie-parser');
 var Locales= require('../');
 Locales.configure({ 
 	dict: path.resolve(__dirname,'./locales'), 
 	langs: ['en', 'zh'], 
-	defaultLang: 'en'  
+	defaultLang: 'en',
+	session: 'locale'
 })
 
 describe('API', function(){
@@ -60,4 +62,40 @@ describe('API', function(){
 			.expect(200, done);
 		
 	})
+
+	it('should use session settings', function (done) {
+		var app = express();
+		var session = require('express-session');
+
+		app.use(session({ 
+		    cookie:{ path: '/', httpOnly: true, maxAge: null}, 
+		    secret: "xyz123", 
+		    key: 'express.sid'
+		}));
+		app.use(cookieParser());
+
+
+		app.get('/setSession', function (req, res) {
+			req.session.locale= 'zh';
+			res.end();
+		})
+
+		app.get('/locales', function(req, res){
+			Locales.load('index_out')(req, res, function() {
+				res.locals.lang.should.be.equal('zh');
+				res.end();
+			})
+		});
+
+		var agent = request.agent(app);
+
+		agent.get('/setSession')
+			.expect(200, function () {
+				agent.get('/locales')
+					.set('Accept-Language', 'ja;q=.8, fr;q=.6')
+					.expect(200, done);
+			});
+		
+	})
+
 })
