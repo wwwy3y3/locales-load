874ae79983165eae7b83a3393cfe68698f29f164
william <wwwy3y3@gmail.com>
2015-03-15 20:57:23 +0800

    add more tests

--- test/api.js
+++ test/api.js
@@ -1,5 +1,6 @@
 var path= require('path');
 var express= require('express');
+var should= require('should');
 var request= require('supertest');
 var Locales= require('../');
 Locales.configure({ 
@@ -9,12 +10,12 @@ Locales.configure({
 })
 
 describe('API', function(){
-	it('store write dict set in res.locals', function (done) {
+	it('should get en dict', function (done) {
 		var app = express();
 
 		app.use(function(req, res){
 			Locales.load('index_out')(req, res, function() {
-				console.log(res.locals);
+				res.locals.lang.should.be.equal('en');
 				res.end();
 			})
 		});
@@ -22,6 +23,40 @@ describe('API', function(){
 		request(app)
 			.get('/')
 			.set('Accept-Language', 'en;q=.5, en-us')
+			.expect(200, done);
+		
+	})
+
+	it('should get zh dict', function (done) {
+		var app = express();
+
+		app.use(function(req, res){
+			Locales.load('index_out')(req, res, function() {
+				res.locals.lang.should.be.equal('zh');
+				res.end();
+			})
+		});
+
+		request(app)
+			.get('/')
+			.set('Accept-Language', 'zh;q=.8, en-us;q=.6')
+			.expect(200, done);
+		
+	})
+
+	it('should fallback to defaultLang- en', function (done) {
+		var app = express();
+
+		app.use(function(req, res){
+			Locales.load('index_out')(req, res, function() {
+				res.locals.lang.should.be.equal('en');
+				res.end();
+			})
+		});
+
+		request(app)
+			.get('/')
+			.set('Accept-Language', 'ja;q=.8, fr;q=.6')
 			.expect(200, done);
 		
 	})
